1cbcbc01ca294ef9d48dfb88582a9d91cb7aecf4
David Storm <jan.david.storm@gmail.com>
2014-07-19 23:23:28 +0200

    Add block explorer

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -21,9 +21,9 @@ nets = dict(
         BLOCK_PERIOD=24, # s
         SYMBOL='VIA',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Viacoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Viacoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.viacoin'), 'viacoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='',
-        ADDRESS_EXPLORER_URL_PREFIX='',
-        TX_EXPLORER_URL_PREFIX='',
+        BLOCK_EXPLORER_URL_PREFIX='http://viacoinexplorer.com/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://viacoinexplorer.com/address/',
+        TX_EXPLORER_URL_PREFIX='http://viacoinexplorer.com/tx/',
         SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
