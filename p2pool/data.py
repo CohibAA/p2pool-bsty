8ba073b43b06d9747b433381dc8ac03055a30f05
David Storm <jan.david.storm@gmail.com>
2014-08-01 20:53:22 +0200

    Use desired_timestamp if previous share is too old

--- p2pool/data.py
+++ p2pool/data.py
@@ -172,16 +172,21 @@ class Share(object):
             raise ValueError()
         
         dests = sorted(amounts.iterkeys(), key=lambda script: (script == DONATION_SCRIPT, amounts[script], script))[-4000:] # block length limit, unlikely to ever be hit
+
+        if previous_share is None or (desired_timestamp - previous_share.timestamp) > 3600:
+            timestamp = desired_timestamp
+        else: 
+            timestamp = math.clip(desired_timestamp, (
+                previous_share.timestamp + 1,
+                previous_share.timestamp + net.SHARE_PERIOD + net.SHARE_PERIOD - 1
+            ))
         
         share_info = dict(
             share_data=share_data,
             far_share_hash=None if last is None and height < 99 else tracker.get_nth_parent_hash(share_data['previous_share_hash'], 99),
             max_bits=max_bits,
             bits=bits,
-            timestamp=math.clip(desired_timestamp, (
-                (previous_share.timestamp + net.SHARE_PERIOD) - (net.SHARE_PERIOD - 1), # = previous_share.timestamp + 1
-                (previous_share.timestamp + net.SHARE_PERIOD) + (net.SHARE_PERIOD - 1),
-            )) if previous_share is not None else desired_timestamp,
+            timestamp=timestamp,
             new_transaction_hashes=new_transaction_hashes,
             transaction_hash_refs=transaction_hash_refs,
             absheight=((previous_share.absheight if previous_share is not None else 0) + 1) % 2**32,
